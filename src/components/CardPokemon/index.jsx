6e179dbafac16f236ad83d40350d7c9791e8cfa8
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-09 00:03:58 -0300

    [new]: Pokemon list with links to each pokemon

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -1,5 +1,85 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import TypeColor from "../../utils/TypeColor"
+import { capitalize } from "../../utils/StringFunctions"
+import { Grid } from "react-loader-spinner"
+import { Link } from "react-router-dom"
 
 export default function CardPokemon(props) {
-	return <div>{String(props.name)}</div>
+	const [pokemon, setPokemon] = useState({})
+	const [typeColor, setTypeColor] = useState("")
+	const [url, setUrl] = useState("")
+
+	useEffect(() => {
+		fetch(props.api)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data)
+				setPokemon(data)
+				setTypeColor(TypeColor[data.types[0].type.name])
+				setUrl(`./pokemon/${data.id}`)
+			})
+	}, [props.api])
+
+	return (
+		<Link
+			style={{
+				textDecoration: "none",
+				color: "inherit",
+				display: "flex",
+			}}
+			to={url}
+		>
+			<div
+				style={{
+					backgroundColor: typeColor,
+					borderRadius: "10px",
+					boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+					display: "flex",
+					flexDirection: "column",
+					padding: "10px",
+					width: "150px",
+				}}
+			>
+				<h2>{pokemon.name ? capitalize(pokemon.name) : props.name}</h2>
+				<div
+					style={{
+						display: "flex",
+						flexWrap: "wrap",
+						gap: "5px",
+					}}
+				>
+					{pokemon.types ? (
+						pokemon.types.map((type, index) => (
+							<span
+								key={index}
+								style={{
+									backgroundColor: "#fff4",
+									border: "1px solid #fff",
+									borderRadius: "100px",
+									padding: "5px 15px",
+								}}
+							>
+								{capitalize(type.type.name)}
+							</span>
+						))
+					) : (
+						<Grid
+							height="40"
+							width="40"
+							color="#ebebeb"
+							ariaLabel="grid-loading"
+							radius="6"
+							wrapperStyle={{}}
+							wrapperClass=""
+							visible={true}
+						/>
+					)}
+				</div>
+				<img
+					src={pokemon.sprites ? pokemon.sprites.front_default : ""}
+					alt={pokemon.name}
+				/>
+			</div>
+		</Link>
+	)
 }
--- src/components/ListaPokemons/index.jsx
+++ src/components/ListaPokemons/index.jsx
@@ -6,24 +6,34 @@ export default function ListaPokemons() {
 	const [loading, setLoading] = React.useState(true)
 
 	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=200")
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data)
 				setLoading(false)
 				setPokemons(data.results)
 			})
 	}, [])
 
 	return (
-		<>
+		<div
+			style={{
+				display: "flex",
+				flexWrap: "wrap",
+				gap: "5px",
+				justifyContent: "center",
+			}}
+		>
 			{loading ? (
 				<p>Carregando...</p>
 			) : (
-				pokemons.map((pokemon) => (
-					<CardPokemon name={pokemon.name} api={pokemon.url} />
+				pokemons.map((pokemon, index) => (
+					<CardPokemon
+						name={pokemon.name}
+						api={pokemon.url}
+						key={index}
+					/>
 				))
 			)}
-		</>
+		</div>
 	)
 }
